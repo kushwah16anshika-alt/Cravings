464464a38efebfbd4338b121e48555fccb685885
Anshika Kushwaha <kushwah16anshika@gmail.com>
2026-07-31 18:10:29 +0530

    Add Samplerestaurant image to assets

--- client/src/pages/RestaurantDetailsPage.jsx
+++ client/src/pages/RestaurantDetailsPage.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import api from "../config/ApiConfig";
+import api from "../config/api.config";
 import toast from "react-hot-toast";
-import Loader from "../components/Loader";
+import Loader from "../assets/runningLoader.gif";
 
 const RestaurantDetailsPage = () => {
   const { restaurantId } = useParams();
