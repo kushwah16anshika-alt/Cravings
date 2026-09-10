2846fd5be9ecd97e9e0d349737ae965df4b43798
Anshika Kushwaha <kushwah16anshika@gmail.com>
2026-09-10 23:33:04 +0530

    feat: implement admin profile management and configure project infrastructure

--- client/src/config/api.config.js
+++ client/src/config/api.config.js
@@ -1,17 +1,7 @@
-//  import axios from "axios";
-
-// const api = axios.create({
-//   baseURL: "http://localhost:4500",
-//   withCredentials: true,
-// });
-
-// export default api;
-
-
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:4500",
   withCredentials: true,
 });
 
